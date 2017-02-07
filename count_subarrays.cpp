f3d0d8a779066e796dad9dd4a68a681c1fce99c4
saurabh kumar <itsmeonly06@gmail.com>
2017-02-07 22:25:35 +0530

    50 points

--- count_subarrays.cpp
+++ count_subarrays.cpp
@@ -1,1 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+ int T;
+ cin>>T;
+ while(T)
+ {
+     int N;
+     cin>>N;
+     int A[N];
+     for(int i=0;i<N;i++)
+        cin>>A[i];
+        int result=0;
+     for(int L=1;L<=N;L++)
+     {
+         for(int i=0;i<N-L+1;i++)
+         {
+          int j=i+L-1;
+            int flag=0;
+         for(int k=i;k<j;k++)
+         {
+             if(A[k]>A[k+1])
+             {
+                 flag=1;
+                 break;
+             }
+         }
+         if(flag==0)
+            result++;
 
+         }
+
+
+     }
+
+
+
+
+     cout<<result<<endl;
+
+  T--;
+ }
+
+
+
+
+
+    return 0;
+}
